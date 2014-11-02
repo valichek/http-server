99cbf8afdb5465448a444e3cf7c93288776a19e6
Roman Scherer <roman@burningswell.com>
2014-11-02 19:17:33 +0100

    Add :deploy-repositories

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
   :dependencies [[com.stuartsierra/component "0.2.2"]
                  [http-kit "2.1.16"]
                  [org.clojure/clojure "1.6.0"]]
+  :aliases {"lint" ["do" ["whitespace-linter"] ["eastwood"]]
+            "ci" ["do" ["difftest"] ["lint"]]}
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:plugins [[jonase/eastwood "0.1.4"]
                              [lein-difftest "2.0.0"]
-                             [listora/whitespace-linter "0.1.0"]]}}
-  :aliases {"lint" ["do" ["whitespace-linter"] ["eastwood"]]
-            "ci" ["do" ["difftest"] ["lint"]]})
+                             [listora/whitespace-linter "0.1.0"]]}})
