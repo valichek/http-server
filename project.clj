c45ae906cf646f1e242f5658f2b79a48e3f8a452
Roman Scherer <roman@burningswell.com>
2014-11-26 21:10:31 +0100

    Use schema

--- project.clj
+++ project.clj
@@ -6,10 +6,13 @@
   :dependencies [[com.stuartsierra/component "0.2.2"]
                  [http-kit "2.1.19"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.0"]]
-  :aliases {"lint" ["do" ["whitespace-linter"] ["eastwood"]]
-            "ci" ["do" ["difftest"] ["lint"]]}
+                 [org.clojure/tools.logging "0.3.1"]
+                 [prismatic/schema "0.3.3"]]
+  :aliases {"ci" ["do" ["difftest"] ["lint"]]
+            "lint" ["do" ["whitespace-linter"] ["eastwood"]]
+            "test-ancient" ["test"]}
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.1.4"]
+  :eastwood {:exclude-linters [:constant-test]}
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.0"]
                              [lein-difftest "2.0.0"]
                              [listora/whitespace-linter "0.1.0"]]}})
--- src/burningswell/component/http_kit.clj
+++ src/burningswell/component/http_kit.clj
@@ -3,19 +3,46 @@
   (:require [clojure.set :refer [rename-keys]]
             [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
-            [org.httpkit.server :as httpkit]))
+            [org.httpkit.server :as httpkit]
+            [schema.core :as s]))
 
-(defn- http-kit-config [server]
+(def ^:dynamic *defaults*
+  "The default server config."
+  {:bind-address "0.0.0.0"
+   :bind-port 8090
+   :max-body 8388608
+   :max-line 4096
+   :max-ws 4194304
+   :queue-size 20480
+   :thread 4
+   :worker-name-prefix "worker-"})
+
+(def Server
+  "The server schema."
+  {:handler-fn s/Any
+   (s/optional-key :bind-address) s/Str
+   (s/optional-key :bind-port) s/Int
+   (s/optional-key :max-body) s/Int
+   (s/optional-key :max-line) s/Int
+   (s/optional-key :max-ws) s/Int
+   (s/optional-key :queue-size) s/Int
+   (s/optional-key :thread) s/Int
+   (s/optional-key :worker-name-prefix) s/Str
+   s/Keyword s/Any})
+
+(s/defn ^:always-validate http-kit-config
+  "Return the HTTPKit config."
+  [server :- Server]
   (->> {:bind-address :ip
         :bind-port :port}
        (rename-keys server)))
 
-(defn start-server
-  "Start the HTTP Kit server component."
-  [{:keys [handler-fn] :as server}]
+(s/defn ^:always-validate start-server :- Server
+  "Start the HTTP Kit server."
+  [server :- Server]
   (if (:stop-fn server)
     server
-    (let [handler (handler-fn server)
+    (let [handler ((:handler-fn server) server)
           stop-fn (httpkit/run-server handler (http-kit-config server))]
       (assert handler "No Ring handler given.")
       (log/infof "HTTP Kit server started on %s:%s."
@@ -23,9 +50,9 @@
                  (:bind-port server))
       (assoc server :stop-fn stop-fn))))
 
-(defn stop-server
-  "Stop the HTTP Kit server component."
-  [server]
+(s/defn ^:always-validate stop-server :- Server
+  "Stop the HTTP Kit server."
+  [server :- Server]
   (when-let [stop-fn (:stop-fn server)]
     (stop-fn)
     (log/infof "HTTP Kit server stopped on %s:%s."
@@ -39,33 +66,23 @@
   (stop [server]
     (stop-server server)))
 
-(defn http-kit-server
+(s/defn ^:always-validate http-kit-server :- Server
   "Make a new HTTP Kit server component.
 
-  Required config:
+  Required:
 
   :handler-fn - A function of one argument, that gets called with the
   server component and it's dependencies, returning a Ring handler
 
-  Optional HTTP Kit config:
+  Optional:
 
   :bind-address       - Which IP to bind on
   :bind-port          - Which port to listen on
   :max-body           - The max. HTTP body size
   :max-line           - The max. initial HTTP line length
+  :max-ws             - The max. websocket message size
   :queue-size         - max job queued before reject to project self
   :thread             - http worker thread count
   :worker-name-prefix - The prfix used for worker threads"
-  [{:keys [handler-fn ip max-body max-line port queue-size
-           thread worker-name-prefix] :as config}]
-  (map->HTTPKitServer
-   (merge
-    {:bind-address "0.0.0.0"
-     :bind-port 8090
-     :max-body 8388608
-     :max-line 4096
-     :max-ws 4194304
-     :queue-size 20480
-     :thread 4
-     :worker-name-prefix "worker-"}
-    config)))
+  [config :- Server]
+  (map->HTTPKitServer (merge *defaults* config)))
