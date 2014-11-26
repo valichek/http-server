c603b06203bfb999f14bfaf559c4098685df8d6b
Roman Scherer <roman@burningswell.com>
2014-11-26 21:11:11 +0100

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject burningswell/http-kit-component "0.1.4"
+(defproject burningswell/http-kit-component "0.1.5-SNAPSHOT"
   :description "A HTTP Kit server component"
   :url "https://github.com/burningswell/http-kit-component"
   :license {:name "Eclipse Public License"
